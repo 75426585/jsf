3cf63d9d0c42a77d209726ba45654702a102fd07
ji <75426585@qq.com>
2015-05-18 13:36:37 +0800

    修改为系统默认的数据库

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,10 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index(){
-		//var_dump($GLOBALS);exit;
-		$this->load->library('qn');
-		$res = $this->qn->upload('file3');
-		//$res = $this->qn->info('test.jpg','test2.jpg');
-		var_dump($res);exit;
+		$this->load->view('admin/article/add');
 	}
 }
