110b0abd2b788648b07b19f032f5173ac58059db
jishuai <75426585@qq.com>
2016-10-19 17:51:56 +0800

    查看环境配置

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -5,8 +5,15 @@ class Article extends CI_Controller {
 		$posts = $this->db->get_where('js_posts',array('id'=>$post_id))->row_array();
 		$this->db->query('update js_posts set view_count = view_count + 1 where id = '.$post_id);
 		$posts['content'] = htmlspecialchars_decode($posts['content']);
+		//如果是markdown则用markdown解析
+		if($posts['post_type'] == 1){
+			$this->load->library('MdTpl');
+			$posts['content'] = $this->mdtpl->display($posts['content']);
+		}
+
 		$this->load->model('tags_model');
 		$tags = $this->tags_model->get_post_tags($post_id);
+
 		$this->sm->assign('posts',$posts);
 		$this->sm->assign('tags',$tags);
 		$this->sm->view('home/article.html');
