7285b6d8d80b577f1323969461c6db36e9e51855
jishuai <75426585@qq.com>
2016-10-19 10:56:09 +0800

    修改显示

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -3,6 +3,7 @@ class Article extends CI_Controller {
 	public function show($post_id){
 		$post_id = max(intval($post_id),1);
 		$posts = $this->db->get_where('js_posts',array('id'=>$post_id))->row_array();
+		$posts['content'] = htmlspecialchars_decode($posts['content']);
 		$this->load->model('tags_model');
 		$tags = $this->tags_model->get_post_tags($post_id);
 		$this->sm->assign('posts',$posts);
